ddb81471fcc13bd39816c0a46eeff67b4f9968b6
nicklaslanman <nicklasllanman@gmail.com>
2019-10-21 14:06:05 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ import subprocess
 import os
 
 # path to directory
-path = "C:\\Users\\Nick\\Documents\\VS Code\\GreenGitHub\\text.txt"
+path = "C:\\Users\\Nick\\Documents\\VS Code\\GreenGitHub
+
+# path to file
+file_path = "C:\\Users\\Nick\\Documents\\VS Code\\GreenGitHub\\text.txt"
 
 # gets current time and date at execution time
 content = str(datetime.datetime.now())
 
-
-os.chdir( "C:\\Users\\Nick\\Documents\\VS Code\\GreenGitHub" )
+# sets
+os.chdir(path)
 
 # Discard changes and pull from master
 subprocess.run(["git", "reset", "--hard", "origin/master"])
 subprocess.run(["git", "pull", "origin", "master"])
 
 # Edit text file
-f=open(path, "w")
+f=open(file_path, "w")
 f.write(content)
 f.close()
 
