c9a8bf4fa92645d4a261110e49bceabd4c602290
Caleb Reiter <ctr020196@gmail.com>
2020-01-03 11:52:50 -0600

    made some minor changes

--- main.py
+++ main.py
@@ -1,40 +1,46 @@
 import datetime
 import subprocess
 import random
+import os
+import sys
 
-# path to directory
-path = "C:\\Users\\Nick\\Documents\\VS Code\\GreenGitHub"
 
-# path to file
-file_path = "C:\\Users\\Nick\\Documents\\VS Code\\GreenGitHub\\text.txt"
+def main():
+    # path to file
+    file_path = os.path.realpath("text.txt");
 
-# Set max to one for a single commit on each run or 
-# randomly generate a max number of commits each run
-max = 1
-# max = random.randint(1,5)
+    # Set max to one for a single commit on each run or 
+    # randomly generate a max number of commits each run
+    num_args = len(sys.argv)
+    base = int(10 if num_args <= 2 else int(sys.argv[2]))
+    try:
+        count = random.randint(1, 5) if num_args <= 1 else int(sys.argv[1], base)
+    except ValueError as error:
+        print(error)
 
-i = 0
-while i < max:
+    while count > 0:
 
-    # gets current time and date at execution time
-    content = str(datetime.datetime.now())
+        # gets current time and date at execution time
+        content = str(datetime.datetime.now())
 
-    # Discard changes and pull from master
-    subprocess.run(["git", "reset", "--hard", "origin/master"])
-    subprocess.run(["git", "pull", "origin", "master"])
+        # Discard changes and pull from master
+        subprocess.run(["git", "reset", "--hard", "origin/master"])
+        subprocess.run(["git", "pull", "origin", "master"])
 
-    # Edit text file
-    f=open(file_path, "w")
-    f.write(content)
-    f.close()
+        # Edit text file
+        with open(file_path, "w") as file:
+            file.write(content)
 
-    # Git Add
-    subprocess.run(["git", "add", "."])
+        # Git Add
+        subprocess.run(["git", "add", "."])
 
-    # Git Commit
-    subprocess.run(["git", "commit", "-m", "Updated text.txt"])
+        # Git Commit
+        subprocess.run(["git", "commit", "-m", "Updated text.txt"])
 
-    # Git Push
-    subprocess.run(["git", "push"])
+        # Git Push
+        subprocess.run(["git", "push"])
 
-    i += 1
+        count -= 1
+
+if __name__ == "__main__":
+    main()
